cee671e6de3b16f70920388ab088c0e54ac41a39
SK-Washif <washiffishal@gmail.com>
2026-01-28 23:56:55 +0600

    add data and load in server side also show in frontend side

--- career-code-client/src/pages/Home/Home.jsx
+++ career-code-client/src/pages/Home/Home.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Banner from './Banner'
+import HotJobs from './HotJobs'
 
 const Home = () => {
+
+  const jobsPromise = fetch('http://localhost:3000/jobs').then(res => res.json());
+
   return (
     <div>
 
       <Banner></Banner>
+      <Suspense>
+      <HotJobs jobsPromise={jobsPromise}></HotJobs>
+      </Suspense>
 
     </div>
   )
