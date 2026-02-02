90b74afd0d9df9ca3b0c181cbac1aa7db59fd6e0
SK-Washif <washiffishal@gmail.com>
2026-02-03 00:05:11 +0600

    Created my applications page and my application api

--- career-code-client/src/pages/Shared/NavBar.jsx
+++ career-code-client/src/pages/Shared/NavBar.jsx
@@ -18,6 +18,12 @@ const NavBar = () => {
 
   const links = <>
     <li><NavLink to='/'>Home</NavLink></li>
+    {
+    user && <>
+    <li><NavLink to='/myApplications'>My Applications</NavLink></li>
+    </>
+    }
+
       </>
   return (
     <div className="navbar bg-base-100 shadow-sm">
